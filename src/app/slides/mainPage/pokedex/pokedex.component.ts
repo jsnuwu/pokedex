43053b374451f64e321906bb3711085a2ada0edb
Bay, Jason <Jason.Bay@adesso.de>
2025-05-15 09:09:19 +0200

    added grid for pokedex

--- src/app/slides/mainPage/pokedex/pokedex.component.ts
+++ src/app/slides/mainPage/pokedex/pokedex.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../../header/header.component';
 import { BackButtonComponent } from "../back-button/back-button.component";
 
 @Component({
   selector: 'app-pokedex',
-  imports: [HeaderComponent, BackButtonComponent],
+   standalone: true,
+  imports: [CommonModule, HeaderComponent, BackButtonComponent],
   templateUrl: './pokedex.component.html',
-  styleUrl: './pokedex.component.css'
+  styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent {
+  placeholderPokemon = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12];
 
 }
