833a6cc2e5ad82172b2bf176ce100d51de692602
Bay, Jason <Jason.Bay@adesso.de>
2025-05-28 11:07:32 +0200

    added 'pokemon' page with header and Back button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { StartPageComponent } from './slides/start-page/start-page.component';
 import { PokedexComponent } from './slides/mainPage/pokedex/pokedex.component';
+import { PokemonComponent } from './slides/services/pokemon/pokemon.component';
 
 export const routes: Routes = [
   { path: '', component: StartPageComponent },
-  { path: 'pokedex', component: PokedexComponent }
+  { path: 'pokedex', component: PokedexComponent },
+  { path: 'pokemon', component: PokemonComponent}
   
 ];
--- src/app/slides/services/pokemon/pokemon.component.ts
+++ src/app/slides/services/pokemon/pokemon.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../header/header.component";
+import { BackButtonComponent } from "../../mainPage/back-button/back-button.component";
 
 @Component({
   selector: 'app-pokemon',
-  imports: [],
+  imports: [HeaderComponent, BackButtonComponent],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
