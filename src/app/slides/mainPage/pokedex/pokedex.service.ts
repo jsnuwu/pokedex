bf1b3a8fc28c5f308afda102572be568619097f0
Leon Harrer <Harrer@gtue.de>
2025-08-07 14:22:05 +0200

    Anzeigen der Pokémon funktioniert

---
+++ src/app/slides/mainPage/pokedex/pokedex.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon} from '../../../shared/models/pokemon.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class PokedexService {
+  private backendUrl = 'http://localhost:5258/Pokemon';
+
+  constructor(private http: HttpClient) {}
+
+  getAllPokemon(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(this.backendUrl);
+  }
+}
