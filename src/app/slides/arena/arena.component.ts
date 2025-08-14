79951505b12d2fecfaaebad0ae8cc5257aaf3add
Bay, Jason <Jason.Bay@adesso.de>
2025-08-14 13:29:01 +0200

    W

--- src/app/slides/arena/arena.component.ts
+++ src/app/slides/arena/arena.component.ts
@@ -1,71 +1,74 @@
 import { Component } from '@angular/core';
-import { BackButtonComponent } from "../mainPage/back-button/back-button.component";
+import { BackButtonComponent } from '../mainPage/back-button/back-button.component';
 
 @Component({
   selector: 'app-arena',
   templateUrl: './arena.component.html',
   styleUrls: ['./arena.component.css'],
-  imports: [BackButtonComponent]
+  imports: [BackButtonComponent],
 })
 export class ArenaComponent {
-
   player = {
     name: 'Pikachu',
     hp: 100,
     maxHp: 100,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
   };
 
   enemy = {
     name: 'Charmander',
     hp: 100,
     maxHp: 100,
-    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
+    img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
   };
 
   log: string[] = [];
   gameOver = false;
   playerWon = false;
-playerShaking = false;
-enemyShaking = false;
+  playerShaking = false;
+  enemyShaking = false;
 
-attack(attacker: any, defender: any, isPlayer: boolean = true) {
-  if (this.gameOver) return;
+  attack(attacker: any, defender: any, isPlayer: boolean = true) {
+    if (this.gameOver) return;
 
-  const damage = Math.floor(Math.random() * 20) + 5;
-  defender.hp -= damage;
-  if (defender.hp < 0) defender.hp = 0;
-  this.log.push(`${attacker.name} greift ${defender.name} an und verursacht ${damage} Schaden!`);
+    const damage = Math.floor(Math.random() * 20) + 5;
+    defender.hp -= damage;
+    if (defender.hp < 0) defender.hp = 0;
+    this.log.push(
+      `${attacker.name} attacks ${defender.name} and deals ${damage} damage!`
+    );
 
-  if (defender === this.player) {
-    this.playerShaking = true;
-    setTimeout(() => this.playerShaking = false, 300);
-  } else {
-    this.enemyShaking = true;
-    setTimeout(() => this.enemyShaking = false, 300);
+    if (defender === this.player) {
+      this.playerShaking = true;
+      setTimeout(() => (this.playerShaking = false), 300);
+    } else {
+      this.enemyShaking = true;
+      setTimeout(() => (this.enemyShaking = false), 300);
+    }
+
+    this.checkGameOver();
+
+    if (isPlayer && !this.gameOver && defender.hp > 0) {
+      setTimeout(() => {
+        const enemyDamage = Math.floor(Math.random() * 20) + 5;
+        attacker.hp -= enemyDamage;
+        if (attacker.hp < 0) attacker.hp = 0;
+        this.log.push(
+          `${defender.name} attacks ${attacker.name} back and deals ${enemyDamage} damage!`
+        );
+
+        if (attacker === this.player) {
+          this.playerShaking = true;
+          setTimeout(() => (this.playerShaking = false), 300);
+        } else {
+          this.enemyShaking = true;
+          setTimeout(() => (this.enemyShaking = false), 300);
+        }
+
+        this.checkGameOver();
+      }, 500);
+    }
   }
-
-  this.checkGameOver();
-
-  if (isPlayer && !this.gameOver && defender.hp > 0) {
-    setTimeout(() => {
-      const enemyDamage = Math.floor(Math.random() * 20) + 5;
-      attacker.hp -= enemyDamage;
-      if (attacker.hp < 0) attacker.hp = 0;
-      this.log.push(`${defender.name} greift ${attacker.name} zurück und verursacht ${enemyDamage} Schaden!`);
-
-      if (attacker === this.player) {
-        this.playerShaking = true;
-        setTimeout(() => this.playerShaking = false, 300);
-      } else {
-        this.enemyShaking = true;
-        setTimeout(() => this.enemyShaking = false, 300);
-      }
-
-      this.checkGameOver();
-    }, 500); 
-  }
-}
 
   checkGameOver() {
     if (this.player.hp <= 0 || this.enemy.hp <= 0) {
